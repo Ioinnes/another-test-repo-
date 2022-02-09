7985aa7747f9eb25ecd7118bc8fce9094e5ef295
Ioinnes <kripakov.id@phystech.edu>
2022-02-09 23:51:13 +0300

    created project

---
+++ cmake-build-debug/CMakeFiles/test_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_project.dir/main.cpp.obj"
+  "libtest_project.dll.a"
+  "test_project.exe"
+  "test_project.exe.manifest"
+  "test_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
